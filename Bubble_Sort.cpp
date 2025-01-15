b696c6d44ba90122de7948a42c3a8333454a15ca
AshutoshSingrole <ashutoshsingrole1@gmail.com>
2025-01-15 10:42:35 +0530

    Create Bubble_Sort.cpp

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	cout << "enter the number of terms : ";
+	cin >> n;
+	
+	int array[n];
+	for(int i = 1 ; i <= n ; i++){
+		cout << "enter the " << i << "th term \n";
+		cin >> array[i];
+	}
+	
+	int temp;
+	
+	for (int i = 1 ; i < n ; i++){
+		for(int j = 1 ; j < n ; j++){
+			if(array[j] > array[j+1]){
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+			
+		}
+	}
+	
+	cout << endl;
+	
+	for(int i = 1 ; i <= n ; i++){
+		cout << array[i] << "\n";
+	}
+	
+	cout << endl;
+	cout << "The higest term is " << array[n];
+	
+	return 0;
+}
