4fdb9e9b1a551a502938d8a92b70a9d5b8868523
AshutoshSingrole <ashutoshsingrole1@gmail.com>
2025-01-15 10:48:18 +0530

    Update Bubble_Sort.cpp

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -15,8 +15,8 @@ int main(){
 	
 	int temp;
 	
-	for (int i = 1 ; i < n ; i++){
-		for(int j = 1 ; j < n ; j++){
+	for (int i = 0 ; i < n-1 ; i++){
+		for(int j = 1 ; j < n -i ; j++){
 			if(array[j] > array[j+1]){
 				temp = array[j];
 				array[j] = array[j+1];
